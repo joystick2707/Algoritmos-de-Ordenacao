60991274af0f3c622f3d317ce28634b9005f7d88
Bryan Strey <137365683+joystick2707@users.noreply.github.com>
2025-06-11 14:19:34 -0300

    falta apenas o bubble sort para ter todos eles

--- Main.java
+++ Main.java
@@ -14,24 +14,17 @@ public class Main {
 
                 System.out.println("Arquivo: " + arquivo);
 
-                // Bubble Sort
-                int[] copia1 = MyArray.copiar(dados);
-                long inicio = System.nanoTime();
-                SortAlgorithms.bubbleSort(copia1);
-                long fim = System.nanoTime();
-                System.out.println(new SortResult(arquivo, "Bubble Sort", fim - inicio));
-
                 // Insertion Sort
-                int[] copia2 = MyArray.copiar(dados);
-                inicio = System.nanoTime();
-                SortAlgorithms.insertionSort(copia2);
-                fim = System.nanoTime();
+                int[] copia = MyArray.copiar(dados);
+                long inicio = System.nanoTime();
+                SortAlgorithms.insertionSort(copia);
+                long fim = System.nanoTime();
                 System.out.println(new SortResult(arquivo, "Insertion Sort", fim - inicio));
 
                 // Quick Sort
-                int[] copia3 = MyArray.copiar(dados);
+                int[] copia2 = MyArray.copiar(dados);
                 inicio = System.nanoTime();
-                SortAlgorithms.quickSort(copia3, 0, copia3.length - 1);
+                SortAlgorithms.quickSort(copia2, 0, copia2.length - 1);
                 fim = System.nanoTime();
                 System.out.println(new SortResult(arquivo, "Quick Sort", fim - inicio));
 
