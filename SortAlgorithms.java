97839849bf152f503179bfdfe41b3fef8a609c6e
Bryan Strey <137365683+joystick2707@users.noreply.github.com>
2025-06-11 14:13:01 -0300

    Create SortAlgorithms.java

---
+++ SortAlgorithms.java
@@ -0,0 +1,57 @@
+public class SortAlgorithms {
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean trocou;
+        for (int i = 0; i < n - 1; i++) {
+            trocou = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    trocou = true;
+                }
+            }
+            if (!trocou) break;
+        }
+    }
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int chave = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > chave) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = chave;
+        }
+    }
+
+    public static void quickSort(int[] arr, int inicio, int fim) {
+        if (inicio < fim) {
+            int p = particionar(arr, inicio, fim);
+            quickSort(arr, inicio, p - 1);
+            quickSort(arr, p + 1, fim);
+        }
+    }
+
+    private static int particionar(int[] arr, int inicio, int fim) {
+        int pivo = arr[fim];
+        int i = inicio - 1;
+        for (int j = inicio; j < fim; j++) {
+            if (arr[j] < pivo) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[fim];
+        arr[fim] = temp;
+        return i + 1;
+    }
+}
