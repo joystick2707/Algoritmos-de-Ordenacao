4f563c8c49644b721c38fd7c9eaf9cb969bd6536
vitin2212 <137333989+vitin2212@users.noreply.github.com>
2025-06-17 17:57:10 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,25 +10,25 @@ public class Main {
                 "crescente_100.csv", "crescente_1000.csv", "crescente_10000.csv",
                 "decrescente_100.csv", "decrescente_1000.csv", "decrescente_10000.csv"
         };
- // Loop para processar cada arquivo da lista
+ // Laço que vai ler e ordenar os dados de cada arquivo, um por vez
         for (String arquivo : arquivos) {
             try {
                  // Lê os dados do arquivo e armazena em um array de inteiros
                 int[] dados = FileReaderUtil.readIntegersAsArray(caminhoPasta + arquivo);
 
-                System.out.println("Arquivo: " + arquivo);
+                System.out.println("Arquivo: " + arquivo); //Exibe qual arquivo está sendo processado
 
                 // Insertion Sort
-                int[] copia = MyArray.copiar(dados);
+                int[] copia = MyArray.copiar(dados); //Faz uma cópia dos dados 
                 long inicio = System.nanoTime();
-                SortAlgorithms.insertionSort(copia);
+                SortAlgorithms.insertionSort(copia); //Chama o método que ordena o array usando o Insertion Sort
                 long fim = System.nanoTime();
                 System.out.println(new SortResult(arquivo, "Insertion Sort", fim - inicio));
 
                 // Quick Sort
                 int[] copia2 = MyArray.copiar(dados);
-                inicio = System.nanoTime();
-                SortAlgorithms.quickSort(copia2, 0, copia2.length - 1);
+                inicio = System.nanoTime(); 
+                SortAlgorithms.quickSort(copia2, 0, copia2.length - 1); //O array, O índice inicial 0 e índice final (ulti posição do array) 
                 fim = System.nanoTime();
                 System.out.println(new SortResult(arquivo, "Quick Sort", fim - inicio));
 
