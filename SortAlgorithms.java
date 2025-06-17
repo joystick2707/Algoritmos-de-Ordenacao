555b310ad02d1ee840c885433e3c3aedbe8f7e92
Bryan Strey <137365683+joystick2707@users.noreply.github.com>
2025-06-17 18:19:10 -0300

    Update SortAlgorithms.java

--- SortAlgorithms.java
+++ SortAlgorithms.java
@@ -1,26 +1,24 @@
 public class SortAlgorithms {
-// Método para ordenar um array usando o algoritmo Insertion Sort
     public static void insertionSort(int[] arr) {
-        int n = arr.length; //Pega o tamanho do array
+        int n = arr.length; 
         for (int i = 1; i < n; ++i) { // Começa a partir do segundo elemento (posição 1) até o final
-            int chave = arr[i]; // Guarda o valor atual (o que vamos posicionar)
+            int chave = arr[i]; 
             int j = i - 1; // Começa a comparar com os elementos anteriores
             while (j >= 0 && arr[j] > chave) {  // Enquanto o valor anterior for maior, vai empurrando os elementos pra frente
                 arr[j + 1] = arr[j]; // Faz o "shift" (move os valores)
                 j = j - 1;  	
             }
-            // Quando achar o lugar certo, coloca a "chave"
             arr[j + 1] = chave;
         }
     }
     public static void quickSort(int[] arr, int inicio, int fim) {
-        if (inicio < fim) { //Condição de parada da recursão
+        if (inicio < fim) { 
             int p = particionar(arr, inicio, fim); // 	Divide o array com base no pivô
             quickSort(arr, inicio, p - 1); // Ordena a parte esquerda
             quickSort(arr, p + 1, fim); // Ordena a parte direita
         }
     }
-// Método auxiliar para particionar o array (usado no Quick Sort)
+// (usado no Quick Sort)
     private static int particionar(int[] arr, int inicio, int fim) {
         int pivo = arr[fim]; // Escolhe o pivô (último elemento do intervalo)
         int i = inicio - 1; 
@@ -43,8 +41,8 @@ public class SortAlgorithms {
     public static void bubbleSort(int[] arr) {
         int n = arr.length;
         boolean trocou;
-        for (int i = 0; i < n - 1; i++) { // 	Faz várias passadas pelo array
-            trocou = false; //Flag para ver se teve alguma troca nesta passada
+        for (int i = 0; i < n - 1; i++) { // Faz várias passadas pelo array
+            trocou = false; 
             // Percorre o array comparando pares de elementos
             for (int j = 0; j < n - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) { // Se os dois estão fora de ordem, troca eles de lugar
@@ -52,10 +50,9 @@ public class SortAlgorithms {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
-                    trocou = true; //	Marca que houve pelo menos uma troca
+                    trocou = true; 
                 }
             }
-            // Se não teve nenhuma troca na passada, o array já está ordenado
             if (!trocou) {
                 break;
             }
